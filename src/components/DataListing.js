cc6edd2edf1758d7a8203e99686690d89b3babc9
KatIsCoding <fabrigoru@gmail.com>
2021-11-16 16:00:14 -0600

    Refactor DataListing component

--- src/components/DataListing.js
+++ src/components/DataListing.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Card from "react-bootstrap/Card"
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getCountryData } from "../redux/cases/cases";
 
 export default function ListComponent({ dates }) {
-    
+      const dispatch = useDispatch();
       return Object.keys(dates).map(date => (
           Object.keys(dates[date].countries).map(countryName => {
             const countryObject = dates[date].countries[countryName]
             return (
-            <Link to={`details/${countryObject.id}`} key={countryObject.id}>
-            <Card style={{width: "10rem"}}>
+            <Link to={`details/${countryObject.id}`} key={countryObject.id} class="dataList" onClick={() => {dispatch(getCountryData(countryObject.id))}}>
+            <Card style={{maxHeight: "179px"}}>
               <Card.Body>
                 <Card.Title>{countryName}</Card.Title>
                 <Card.Subtitle>Confirmed Today: {countryObject.today_confirmed}</Card.Subtitle>
