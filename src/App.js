130fdc41f27ad65f45881d1974bf1d01b1a4921e
KatIsCoding <fabrigoru@gmail.com>
2021-11-17 17:24:32 -0600

    Fix Navbar back button bug

--- src/App.js
+++ src/App.js
@@ -2,27 +2,37 @@ import React, { useEffect } from 'react';
 import './App.css';
 import Navbar from "react-bootstrap/Navbar"
 import { HiArrowSmLeft } from "react-icons/hi"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route ,useNavigate} from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import Home from "./components/Home"
 import Details from "./components/Details"
 import { getCasesFunction } from "./redux/cases/cases";
 
+const NavigationBar = () => {
+  const history = useNavigate();
+  return (
+  <Navbar bg="dark" variant="dark">
+      <HiArrowSmLeft className="navbar-brand" size={40} onClick={() => history(-1)}/>
+      <Navbar.Brand href="#home">
+        Covid-19 Tracker
+      </Navbar.Brand>
+    </Navbar>
+  )
+}
+
+
 const App = () => {
   const dispatch = useDispatch();
   const { total } = useSelector(state => state);
+  
   useEffect(() => {
     dispatch(getCasesFunction());
   }, [dispatch]);
   return (
   <main>
-    <Navbar bg="dark" variant="dark">
-      <HiArrowSmLeft className="navbar-brand" size={40} />
-      <Navbar.Brand href="#home">
-        Covid-19 Tracker
-      </Navbar.Brand>
-    </Navbar>
     <Router>
+    <NavigationBar />
+    
       <Routes>
         <Route exact path="/" element={<Home total={total}/>} />
         <Route path="/details/:countryId" element={<Details />}/>
