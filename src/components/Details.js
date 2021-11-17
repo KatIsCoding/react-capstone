2b5bf85341e7b633adcafca04c8259b504a60cb4
KatIsCoding <fabrigoru@gmail.com>
2021-11-17 14:53:38 -0600

    Getting data from the already retrieved data instead of fetching it again by country

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+/* eslin-disable no-unused-vars */
+
+import React, { useEffect } from 'react';
+import { useSelector , useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { Container, ListGroup } from 'react-bootstrap';
-// import { getCountryData } from '../redux/cases/cases';
-
+import { getCountryDataAction } from '../redux/cases/cases';
 // const currentID = "";
 
 export default function CountryDetails() {
+
   const { countryId } = useParams();
-  // const dispatch = useDispatch();
-  const { countryData } = useSelector((state) => state);
-  const date = Object.keys(countryData.dates)[0];
-  const countryObject = countryData.dates[date].countries[Object.keys(countryData.dates[date].countries)[0]];
+  const dispatch = useDispatch();
+  const countryObject = useSelector((state) => state.countryData);
+  // let countryObject = {};
+
+  
+  useEffect(() => {
+    dispatch(getCountryDataAction(countryId))
+  }, [dispatch, countryId]) 
+  
     return (
       <Container key={countryId}>
       <h1>Details of: {countryObject.name}</h1>
@@ -22,7 +29,7 @@ export default function CountryDetails() {
         <ListGroup.Item>Recovered Today: {countryObject.today_recovered}</ListGroup.Item>
       
     </ListGroup>
-    </Container>);
-  // return (<div>Loading...</div>);
+    </Container>);    
+
 
 }
