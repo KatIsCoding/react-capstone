3fccdf8c18168ca9c68d0f7ab5fd6237b33f258c
KatIsCoding <fabrigoru@gmail.com>
2021-11-18 18:18:11 -0600

    Added Chrome Devtools Utility

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './cases/cases';
 
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk, logger)));
 
 export default store;
